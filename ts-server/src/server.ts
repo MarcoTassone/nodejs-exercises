768aca5e4c8de7cd492841930a53c08433d4a4dc
Marco <tassonemarco67@gmail.com>
2023-12-01 17:48:22 +0100

    added add-psql-db exercise

---
+++ ts-server/src/server.ts
@@ -0,0 +1,18 @@
+import express from "express";
+const app = express();
+app.use(express.json());
+import { getAll, getOneById, create, updateById, deleteById } from "./controllers/planets.js";
+
+app.get("/api/planets", getAll)
+
+app.get("/api/planets/:id", getOneById)
+
+app.post("/api/planets", create)
+
+app.put("/api/planets/:id", updateById)
+
+app.delete("/api/planets/:id", deleteById)
+
+app.listen(3000, () => {
+  console.log("the server is running on http://localhost:3000");
+})
