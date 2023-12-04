af4feb288abb122338fcce5bbeb0d703b8de8568
Marco <tassonemarco67@gmail.com>
2023-12-04 22:22:16 +0100

    added jwt-login-and-signup exercise

--- ts-server/dist/server.js
+++ ts-server/dist/server.js
@@ -2,6 +2,7 @@ import express from "express";
 const app = express();
 app.use(express.json());
 import multer from "multer";
+import { login, signup } from "./controllers/users.js";
 import { getAll, getOneById, create, updateById, deleteById, uploadImage } from "./controllers/planets.js";
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -18,6 +19,8 @@ app.post("/api/planets", create);
 app.put("/api/planets/:id", updateById);
 app.delete("/api/planets/:id", deleteById);
 app.post("/api/planets/:id/image", upload.single("image"), uploadImage);
+app.post("/api/users/login", login);
+app.post("/api/users/signup", signup);
 app.listen(3000, () => {
     console.log("the server is running on http://localhost:3000");
 });
--- ts-server/src/server.ts
+++ ts-server/src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 const app = express();
 app.use(express.json());
 import multer from "multer";
+import { login, signup } from "./controllers/users.js"
 import { getAll, getOneById, create, updateById, deleteById, uploadImage } from "./controllers/planets.js";
 
 const storage = multer.diskStorage({
@@ -25,6 +26,10 @@ app.put("/api/planets/:id", updateById)
 app.delete("/api/planets/:id", deleteById)
 
 app.post("/api/planets/:id/image", upload.single("image"), uploadImage)
+
+app.post("/api/users/login", login)
+
+app.post("/api/users/signup", signup)
 
 app.listen(3000, () => {
   console.log("the server is running on http://localhost:3000");
